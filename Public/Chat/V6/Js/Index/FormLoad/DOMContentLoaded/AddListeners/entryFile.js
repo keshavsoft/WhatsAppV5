01924b49049678d582c31d6173230d01ec1490a8
Durga Prasad Kakileti <durgaprasad@pydahsoft.in>
2025-08-22 17:08:16 +0530

    Update entryFile.js

--- Public/Chat/V6/Js/Index/FormLoad/DOMContentLoaded/AddListeners/entryFile.js
+++ Public/Chat/V6/Js/Index/FormLoad/DOMContentLoaded/AddListeners/entryFile.js
@@ -1,7 +1,7 @@
 import { StartFunc as StartFuncAddUserId } from "./AddUserId/entryFile.js";
 import { StartFunc as StartFuncChangeNameId } from "./ChangeNameId/entryFile.js";
 import { StartFunc as StartFuncShowUsersId } from "./ShowUsersId/entryFile.js";
-import { StartFunc as StartFuncFromShowChatId } from './ShowChatId/EntryFile.js';
+import { StartFunc as StartFuncFromShowChatId } from './ShowChatId/entryFile.js';
 
 let StartFunc = () => {
     StartFuncAddUserId();
